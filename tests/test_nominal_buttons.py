a330fe953bad5343dcea2bb7ef0d65d4514b0a59
id301 <mam.nik.al@gmail.com>
2022-10-22 23:52:47 +0300

    allure decorators added

--- tests/test_nominal_buttons.py
+++ tests/test_nominal_buttons.py
@@ -1,7 +1,15 @@
 __author__ = 'id301'
 
+import allure
+
+@allure.feature('Кнопки с номиналом карты')
 def test_nominal_buttons(app):
-    for button in app.nominal_div.get_buttons():
-        app.nominal_div.click_button(button)
-        assert app.nominal_div.get_button_value(button) == app.nominal_div.get_input_value()
-        assert app.nominal_div.is_button_active(button)
+    with allure.step('Для каждой кнопки с номиналом'):
+        for button in app.nominal_div.get_buttons():
+            button_value = app.nominal_div.get_button_value(button)
+            with allure.step(f'Кнопка с номиналом {button_value}: Нажатие на кнопку'):
+                app.nominal_div.click_button(button)
+            with allure.step(f'Кнопка с номиналом {button_value}: Сравнение с номиналом в поле "Введите"'):
+                assert button_value == app.nominal_div.get_input_value()
+            with allure.step(f'Кнопка с номиналом {button_value}: Проверка состояния "активности" кнопки'):
+                assert app.nominal_div.is_button_active(button)
