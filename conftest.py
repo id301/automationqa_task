a73a1b91f809ebf336e2f21a0358a340a3bda1ea
id301 <mam.nik.al@gmail.com>
2022-10-22 22:45:27 +0300

    config added

--- conftest.py
+++ conftest.py
@@ -1,14 +1,23 @@
 __author__ = 'id301'
 
 import pytest
+import json
+import os.path
 from fixture.application import Application
 
 
 @pytest.fixture
 def app(request):
-    fixture = Application('qa.digift.ru/')
-    fixture.session.login('HR', 'test')
+    config = load_confing()
+    fixture = Application(config['baseUrl'])
+    fixture.session.login(config['login'], config['password'])
     def fin():
         fixture.destroy()
     request.addfinalizer(fin)
     return fixture
+
+def load_confing():
+    config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config.json')
+    with open(config_file) as f:
+        config = json.load(f)
+    return config
