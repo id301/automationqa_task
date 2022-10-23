c8c7a981f6e264b0a22c0e78e77af67e2f22135a
id301 <mam.nik.al@gmail.com>
2022-10-23 16:25:46 +0300

    add api-helper class

---
+++ fixture/task2/api_requests.py
@@ -0,0 +1,22 @@
+__author__ = 'id301'
+
+import requests
+from model.js_test_task import JsTestTask
+
+
+class ApiRequests:
+
+    def __init__(self, config):
+        self.base_url = config['baseUrl']
+        self.js_test_task_method = config['method']
+        self.search_param = config['search']
+        self.sort_field_param = config['sort_field']
+
+    def get_products(self, search='', sort_field=''):
+        search_text = (self.search_param + search) if search != '' else ''
+        sort_field_text = (self.sort_field_param + sort_field) if sort_field else ''
+        response = requests.get(f'{self.base_url}{self.js_test_task_method}?{search_text}&{sort_field_text}')
+        lst_models = []
+        for product in response.json()['products']:
+            lst_models.append(JsTestTask.parse_obj(product))
+        return lst_models
