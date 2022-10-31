836f6ec610353b8a5ddcef30a14b3f6d0edb018d
id301 <mam.nik.al@gmail.com>
2022-10-31 20:52:28 +0300

    some small changes with page-classes

--- task1/pages/base_page.py
+++ task1/pages/base_page.py
@@ -18,12 +18,16 @@ class BasePage:
         """Открытие страницы с basic auth"""
         self.wd.get(f'http://{login}:{password}@{self.url}')
 
-    def find_element(self, locator, time):
+    def click_button(self, button):
+        """Нажатие на переданную кнопку"""
+        button.click()
+
+    def find_element(self, locator, time=5):
         """Обертка для поиска одного элемента с явным ожиданием"""
         return WebDriverWait(self.wd, time).until(EC.presence_of_element_located(locator),
                                                   message=f'Element with locator {locator} not found')
 
-    def find_elements(self, locator, time):
+    def find_elements(self, locator, time=5):
         """Обертка для поиска нескольких элементов с явным ожиданием"""
         return WebDriverWait(self.wd, time).until(EC.presence_of_all_elements_located(locator),
                                                   message=f'Elements with locator {locator} not found')
--- task1/pages/main_page.py
+++ task1/pages/main_page.py
@@ -8,11 +8,7 @@ class MainPage(BasePage):
 
     def get_nominal_buttons(self):
         """Получение списка кнопок с номиналом"""
-        return self.find_elements(MainPageLocators.NOMINAL_BUTTONS, 5)
-
-    def click_button(self, button):
-        """Нажатие на переданную кнопку"""
-        button.click()
+        return self.find_elements(MainPageLocators.NOMINAL_BUTTONS)
 
     def click_every_button(self, buttons_list):
         """Нажатие на каждую кнопку в переданном списке"""
@@ -26,11 +22,11 @@ class MainPage(BasePage):
 
     def get_nominal_input_value(self):
         """Получение номинала в инпуте"""
-        return self.find_element(MainPageLocators.NOMINAL_INPUT, 5).get_attribute("value")
+        return self.find_element(MainPageLocators.NOMINAL_INPUT).get_attribute("value")
 
     def check_input_equals_nominal_button(self):
         """Проверка номинала в инпуте, сравнение со значением номинала в активной кнопке"""
-        button = self.find_element(MainPageLocators.ACTIVE_NOMINAL_BUTTON, 5)
+        button = self.find_element(MainPageLocators.ACTIVE_NOMINAL_BUTTON)
         button_value = self.get_nominal_button_value(button)
         input_value = self.get_nominal_input_value()
         assert input_value == button_value, f'Значение номинала кнопки {button_value} не соответствует номиналу' \
@@ -38,5 +34,5 @@ class MainPage(BasePage):
 
     def check_is_button_active(self, button):
         """Проверка того, что переданная кнопка активна"""
-        assert button == self.find_element(MainPageLocators.ACTIVE_NOMINAL_BUTTON, 5), \
+        assert button == self.find_element(MainPageLocators.ACTIVE_NOMINAL_BUTTON), \
             f'Кнопка с номиналом {self.get_nominal_button_value(button)} не является активной'
